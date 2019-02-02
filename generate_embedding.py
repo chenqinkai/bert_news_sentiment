eeafe60c55f4c06c75f73524ac21fc2fa9e37ee6
CHEN Qinkai <qinkai.chen@mines-paristech.fr>
2019-02-02 23:39:32 +0100

    reindent and modify start cmd

--- generate_embedding.py
+++ generate_embedding.py
@@ -1,4 +1,5 @@
 from bert_serving.client import BertClient
 
 bc = BertClient()
-a = bc.encode(['First do it', 'then do it right', 'then do it better'])
+a = bc.encode(['First do it', 'then do it right',
+               'then do it better', "qinkai"])
