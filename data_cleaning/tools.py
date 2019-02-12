7122cade22d82370a06c8032dbadc571f378e724
CHEN Qinkai <qinkai.chen@mines-paristech.fr>
2019-02-12 15:01:53 +0800

    reorganize folder and add tools to deal with reuters news

--- data_cleaning/tools.py
+++ data_cleaning/tools.py
@@ -14,3 +14,19 @@ def cat_all_csv(file_dir, save_path):
     df_all.index = range(df_all.shape[0])
     df_all.to_csv(save_path, sep='\t')
     return df_all
+
+
+def cat_all_csv_rt(file_dir, save_path):
+    df_all = []
+    for year in os.listdir(file_dir):
+        # debug
+        # if year != "2018":
+        #     continue
+        for f in os.listdir(os.path.join(file_dir, year)):
+            print(f)
+            path = os.path.join(file_dir, year, f)
+            df = pd.read_csv(path, header=None)
+            df_all += [df]
+    df_all = pd.concat(df_all)
+    df_all.to_csv(save_path, index=False)
+    return df_all
