6257e5a1001730cd52c34d40eafe808ba94002ec
CHEN Qinkai <qinkai.chen@mines-paristech.fr>
2019-02-12 22:09:43 +0800

    add functions to cat csv and label training and test data

--- data_cleaning/tools.py
+++ data_cleaning/tools.py
@@ -8,11 +8,11 @@ def cat_all_csv(file_dir, save_path):
     df_all = []
     for f in os.listdir(file_dir):
         print(f)
-        df = pd.read_csv(os.path.join(file_dir, f), index_col=0, sep='\t')
+        df = pd.read_csv(os.path.join(file_dir, f), index_col=0)
         df_all += [df]
     df_all = pd.concat(df_all)
     df_all.index = range(df_all.shape[0])
-    df_all.to_csv(save_path, sep='\t')
+    df_all.to_csv(save_path)
     return df_all
 
 
@@ -30,3 +30,21 @@ def cat_all_csv_rt(file_dir, save_path):
     df_all = pd.concat(df_all)
     df_all.to_csv(save_path, index=False)
     return df_all
+
+
+def label_training_data(df_news, col_name, percentile):
+    upper_threshold = df_news[col_name].quantile(1 - percentile)
+    lower_threshold = df_news[col_name].quantile(percentile)
+    df_positive = df_news[df_news[col_name] > upper_threshold]
+    df_positive["Label"] = 1
+    df_negative = df_news[df_news[col_name] < lower_threshold]
+    df_negative["Label"] = 0
+    return pd.concat([df_positive, df_negative]).sort_values("Date")
+
+
+def label_test_data(df_news, col_name):
+    df_positive = df_news[df_news[col_name] > 0]
+    df_positive["Label"] = 1
+    df_negative = df_news[df_news[col_name] < 0]
+    df_negative["Label"] = 0
+    return pd.concat([df_negative, df_positive]).sort_values("Date")
