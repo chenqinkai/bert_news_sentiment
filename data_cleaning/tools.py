62c15cb64d2bf5d34d54b3c1231a63929219f527
CHEN Qinkai <qinkai.chen@mines-paristech.fr>
2019-02-15 14:54:41 +0800

    add function to plot accuracy fig

--- data_cleaning/tools.py
+++ data_cleaning/tools.py
@@ -1,5 +1,7 @@
-import pandas as pd
 import os
+
+import numpy as np
+import pandas as pd
 
 TWEET_WITH_RETURN_DIR = "D:/data/bert_news_sentiment/tweet_with_return"
 
@@ -54,3 +56,29 @@ def label_test_data(df_news, col_name):
     df_negative = df_news[df_news[col_name] < 0]
     df_negative["Label"] = 0
     return pd.concat([df_negative, df_positive]).sort_values("Date")
+
+
+def get_accuracy(df_news, predict_col_name, prediction, percentile):
+    """
+        type(prediction): list, which contains the score of the news
+    """
+    assert df_news.shape[0] == len(prediction)
+    df_news["prediction"] = prediction
+    th_negative = np.percentile(prediction, percentile / 2)
+    th_positive = np.percentile(prediction, 100 - percentile / 2)
+    df_partial = df_news[(df_news["prediction"] < th_negative) | (
+        df_news["prediction"] > th_positive)]
+    total = df_partial.shape[0]
+    correct = df_partial[np.sign(df_partial[predict_col_name]) == np.sign(
+        df_partial["prediction"])].shape[0]
+    return correct / float(total)
+
+
+from matplotlib import pyplot as plt
+
+
+def plot_accuracy(df_news, predict_col_name, prediction):
+    s = pd.Series()
+    for p in range(1, 99):
+        s.set_value(p, get_accuracy(df_news, predict_col_name, prediction, p))
+    plt.plot(s)
