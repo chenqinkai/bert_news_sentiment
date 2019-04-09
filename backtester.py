46bcc0ecd61ac5e87f14f673862c2875f11c288d
CHEN Qinkai <chenqinkai@gmail.com>
2019-04-10 01:22:16 +0800

    bug fix for backtest

--- backtester.py
+++ backtester.py
@@ -12,11 +12,12 @@ def load_prediction(prediction_file_path):
     if prediction_file_path.endswith(".tsv"):
         with open(prediction_file_path, 'r') as f:
             df_pred = pd.read_csv(f, sep='\t', header=None)
+        return ((df_pred[1] - 0.5) * 2).tolist()
     elif prediction_file_path.endswith(".npy"):
         df_pred = np.load(prediction_file_path)
+        return (df_pred[:, 1] - 0.5) * 2
     else:
         raise NotImplementedError("only .tsv and .npy are accepted")
-    return (df_pred[1] - 0.5).tolist()
 
 
 def calculate_daily_pnl(df_daily, return_col_name):
@@ -93,17 +94,19 @@ def get_pnl_by_date(df_test_with_position, save_path=None, show_fig=True):
     if show_fig:
         plt.show()
     if save_path:
-        fig.save_fig(save_path)
+        fig.savefig(save_path)
     return indicator_by_date, stats
 
 
 def parse_one_argument(arg):
-    if '-' in arg and arg.counts('-') == 1:
+    if '-' in arg and arg.count('-') == 1:
         return tuple(arg.split('-'))
     if '-' in arg and arg.startswith("lstm-"):
-        return tuple("shape", arg[5:])
+        return tuple(["shape", arg[5:]])
     if arg in ["lstm", "gru", "rnn"]:
         return tuple(["cell_type", arg])
+    if arg in ["bert", "berttuned"]:
+        return tuple(["tuned", arg == "berttuned"])
     raise ValueError("argument not recognized: %s" % arg)
 
 
@@ -113,6 +116,7 @@ def parse_file_argument(file_name):
         bert_label-010_emd-768_maxlen-32_lstm-128-128-128_drop-050_epoch-3_lr-100_batch-128_layer-2
         bert_label-010_emd-768_maxlen-32_lstm-128-64-32_drop-050_epoch-5/
     """
+    file_name = file_name.split('.')[0]
     d = {
         "tuned": False,
         "label": 10,
@@ -142,14 +146,16 @@ def main():
     df_stat = pd.DataFrame()
     df_test = load_test_tsv(
         r"D:\data\reuters_headlines_by_ticker\horizon_3\test_horizon_3.tsv")
-    for file_name in tqdm(os.listdir(PREDICTION_DIR)):
+    for index, file_name in tqdm(enumerate(os.listdir(PREDICTION_DIR))):
         prediction = load_prediction(os.path.join(PREDICTION_DIR, file_name))
         df_test_copy = df_test.copy()
         df_test_copy["Prediction"] = prediction
         df_test_copy = prediction_to_position(df_test_copy)
         pnl_by_date, stats = get_pnl_by_date(df_test_copy, save_path=os.path.join(
             SAVE_DIR, file_name.split('.')[0] + ".png"), show_fig=False)
-        df_stat = df_stat.append(pd.Series(parse_file_argument(file_name)))
+        param = parse_file_argument(file_name)
+        param.update(stats)
+        df_stat = df_stat.append(pd.Series(param, name=index))
     df_stat.to_csv(os.path.join(SAVE_DIR, "summary.csv"))
 
 
